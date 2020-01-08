c02a428e2f1ee104332cb05aa45ae8fd340befee
mercator <sayhallo487@gmail.com>
2020-01-08 23:36:00 +0900

    FloatingActionButton Snackbar add

--- app/src/main/java/com/example/memolistapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/memolistapplication/ui/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.memolistapplication.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.memolistapplication.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<FloatingActionButton>(R.id.float_button).apply {
+            this.setOnClickListener { v->Snackbar.make(v,"floatbuttonnnn!!!",Snackbar.LENGTH_LONG).show() }
+        }
     }
 }
